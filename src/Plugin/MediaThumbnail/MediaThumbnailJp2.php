e9c18588d5237e4820d5a25e5739efdaca2d0891
Jeff He <jeffbuyunhe@hotmail.com>
2023-10-19 19:51:53 -0400

    removed and replaced dep function

--- src/Plugin/MediaThumbnail/MediaThumbnailJp2.php
+++ src/Plugin/MediaThumbnail/MediaThumbnailJp2.php
@@ -54,7 +54,7 @@ class MediaThumbnailJp2 extends MediaThumbnailBase {
     $im->destroy();
 
     // Return a new managed file object using the generated thumbnail.
-    return file_save_data($image, $sourceUri . '.jpg');
+    return \Drupal::service('file.repository')->writeData($image, $sourceUri . '.jpg');
 
   }
 
